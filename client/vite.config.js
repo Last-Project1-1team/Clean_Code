6dc166664e180f75512828e982da3e3bf42ad040
YUHAN KO <rhdbgks789@gmail.com>
2025-09-30 09:21:54 +0900

    250930 세팅 커밋

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,37 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { PrimeVueResolver } from "@primevue/auto-import-resolver";
-import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import { defineConfig } from "vite";
-import VueDevTools from "vite-plugin-vue-devtools";
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
+const server = 'http://localhost:3000';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
-        noDiscovery: true,
+        noDiscovery: true
     },
     plugins: [
         vue(),
         Components({
-            resolvers: [PrimeVueResolver()],
+            resolvers: [PrimeVueResolver()]
         }),
-        VueDevTools(), // ✅ devtools 추가
+        VueDevTools() // ✅ devtools 추가
     ],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
+    server: {
+        port: 8090,
+        proxy: {
+            '^/api': {
+                target: server,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/')
+            }
+        }
+    }
 });
