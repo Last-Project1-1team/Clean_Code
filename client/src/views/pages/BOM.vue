93c10b4e4de5db470321c5214c3fbdbcbc968ae2
HJW <ayyam670@naver.com>
2025-10-02 03:55:23 +0900

    황지원/251002/제품마스터 마무리, BOM,작업지시 화면 작업/ node쪽 오류 있음

---
+++ client/src/views/pages/BOM.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+import ModelMasterSearch from '@/components/ModelMasterSearch.vue';
+</script>
+
+<template>
+    <div class="card">
+        <ModelMasterSearch />
+
+        <TreeTable :value="treeTableValue" selectionMode="checkbox" v-model:selectionKeys="selectedTreeTableValue" style="height: 65vh; border: 1px solid #ddd">
+            <Column field="name" header="level" :expander="true"></Column>
+            <Column field="size" header="품번"></Column>
+            <Column field="type" header="품명"></Column>
+            <Column field="type" header="규격"></Column>
+            <Column field="type" header="수량"></Column>
+            <Column field="type" header="단위"></Column>
+        </TreeTable>
+    </div>
+</template>
