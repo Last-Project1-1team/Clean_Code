47024c53a9cba0995b01dcfb401d1367ba55a428
heochangbeom <heoc48689@gmail.com>
2025-10-13 22:57:25 +0900

    허창범/수주등록-업체모달완료 제품모달 개발중/251013

--- client/src/views/pages/InOrderReg.vue
+++ client/src/views/pages/InOrderReg.vue
@@ -2,54 +2,50 @@
 import { ProductService } from '@/service/ProductService';
 import { FilterMatchMode } from '@primevue/core/api';
 import { useToast } from 'primevue/usetoast';
-import { onMounted, ref, onBeforeMount, shallowRef, computed, reactive } from 'vue';
+import { onMounted, ref } from 'vue';
 import Dialog from 'primevue/dialog';
-// import CustomModal from '@/views/pages/auth/custmodal.vue';
-import CustModal from '@/components/CustSearchModal.vue';
+import CustSearchModal from '@/components/CustSearchModal.vue';
+import ModalSearchModel from '@/components/ModalSearchModel.vue';
 import axios from 'axios';
-// import ModelModal  from
-// import 제품모달창 from '';
-// import 업체모달창 from '';
-// import axios from 'axios';
-// import { onBeforeMount, shallowRef, computed } from 'vue';
-// import useDateUtils from '@/utils/useDates.js';
-
-// 폼 상태
-const form = reactive({
-    CUST_CODE: '',
-    CUST_NAME: '',
-    PSCH_PHONE: ''
-});
 
 const selectedinordmodel = ref([]);
-
+const selectedmodels = ref([]);
+const custCode = ref([]);
+const custName = ref([]);
+const pschphone = ref([]);
+const modelcode = ref([]);
 const selectedRows = ref([]);
+const custmodalVisible = ref(false);
+const modelmodalVisible = ref(false);
+const today = ref(new Date()); // 오늘 날짜
+const selectedDate = ref(new Date(new Date().setDate(new Date().getDate() + 7)));
 
-const custopen = ref(false);
-
-const handleCustRegister = (cust) => {
+const handleCustRegister = (custs) => {
     // 부모 테이블에 추가 (기존 데이터 유지 + 신규 추가)
-    const mapped = cust
-        .filter((cust) => {
-            // 이미 등록된 ITEM_CODE는 제외
-            return !selectedinordmodel.value.some((si) => si.CUST_CODE === cust.custCode);
-        })
-        .map((cust) => ({
-            custCode: cust.CUST_CODE,
-            CUST_NAME: cust.CUST_NAME,
-            PSCH_PHONE: cust.PSCH_PHONE
-        }));
+    custCode.value = custs.custCode;
+    custName.value = custs.custName;
+    pschphone.value = custs.pschPhone;
 
-    selectedinordmodel.value = [...selectedinordmodel.value, ...mapped];
-    selectedinordmodel.value = false; // 모달 닫기
+    custmodalVisible.value = false; // 모달 닫기
 };
 
-// 모달에서 '단일' 고객 선택 이벤트 처리
-const handleCustSelect = (cust) => {
-    form.CUST_CODE = cust.CUST_CODE;
-    form.CUST_NAME = cust.CUST_NAME;
-    form.PSCH_PHONE = cust.PSCH_PHONE;
-    custopen.value = false;
+const handleModelRegister = (models) => {
+    // 부모 테이블에 추가 (기존 데이터 유지 + 신규 추가)
+    const mapped = models
+        .filter((model) => {
+            // 이미 등록된 ITEM_CODE는 제외
+            return !selectedmodels.value.some((si) => si.MODEL_CODE === model.modelCode);
+        })
+        .map((model) => ({
+            MODEL_CODE: model.modelCode,
+            MODEL_NAME: model.modelName,
+            REVISION: model.revision,
+            INORD_QTY: model.inordQty,
+            UNIT: model.unit
+        }));
+
+    selectedmodels.value = [...selectedmodels.value, ...mapped];
+    modelmodalVisible.value = false; // 모달 닫기
 };
 </script>
 
@@ -59,17 +55,13 @@ const handleCustSelect = (cust) => {
             <label for="CUST_CODE" class="flex items-center">업체코드</label>
             <div class="col-span-2">
                 <div class="grid grid-cols-[1fr_auto] gap-2">
-                    <InputText v-model="form.CUST_CODE" class="w-full" />
-                    <Button @click="custopen = true" icon="pi pi-search" class="flex-none" style="width: 2.5rem; height: 2.5rem" />
-
-                    <Dialog v-model:visible="custopen" header="업체 검색" modal style="width: 80vw; height: 80vh">
-                        <CustModal @select="handleCustSelect" @register="handleCustRegister" />
-                    </Dialog>
+                    <InputText v-model="custCode" class="w-full" />
+                    <Button @click="custmodalVisible = true" icon="pi pi-search" class="flex-none" style="width: 2.5rem; height: 2.5rem" />
                 </div>
             </div>
             <label for="form.PSCH_PHONE " class="flex items-center">담당자 연락처</label>
             <div class="col-span-2">
-                <InputText id="PSCH_PHONE" type="text" class="w-full" />
+                <InputText v-model="pschphone" type="text" class="w-full" />
             </div>
             <Button click="" type="submit" class="col-start-12" label="저장" />
         </div>
@@ -77,7 +69,7 @@ const handleCustSelect = (cust) => {
         <div class="grid grid-cols-12 gap-2">
             <label for="form.CUST_NAME" class="flex items-center">업체명</label>
             <div class="col-start-2 col-end-7">
-                <InputText id="PSCH_PHONE" type="text" class="w-full" />
+                <InputText v-model="custName" type="text" class="w-full" />
             </div>
         </div>
 
@@ -85,8 +77,8 @@ const handleCustSelect = (cust) => {
             <label for="MODEL_CODE" class="flex items-center">제품코드</label>
             <div class="col-span-2">
                 <div class="grid grid-cols-[1fr_auto] gap-2">
-                    <InputText v-model="value" class="w-full" />
-                    <Button @click="modelopen = true" icon="pi pi-search" class="flex-none" style="width: 2.5rem; height: 2.5rem" />
+                    <InputText v-model="modelcode" class="w-full" />
+                    <Button @click="modelmodalVisible = true" icon="pi pi-search" class="flex-none" style="width: 2.5rem; height: 2.5rem" />
                 </div>
             </div>
         </div>
@@ -94,14 +86,23 @@ const handleCustSelect = (cust) => {
         <div class="grid grid-cols-12 gap-2">
             <label for="INORD_DATE" class="flex items-center">수주일</label>
             <div class="col-span-2">
-                <InputText id="PSCH_PHONE" type="date" class="w-full" />
+                <DatePicker v-model="today" class="w-full" name="inordDate" dateFormat="yy-mm-dd" showIcon showButtonBar iconDisplay="input" inputId="icondisplay" />
             </div>
             <label for="PAPRD_DATE" class="flex items-center">납기일</label>
             <div class="col-span-2">
-                <InputText id="PSCH_PHONE" type="date" class="w-full" />
+                <DatePicker v-model="selectedDate" class="w-full" name="paprddate" dateFormat="yy-mm-dd" showIcon showButtonBar iconDisplay="input" inputId="icondisplay" />
             </div>
             <Button class="col-start-12" label="Submit">삭제</Button>
         </div>
+
+        <Dialog v-model:visible="custmodalVisible" header="업체 검색" modal style="width: 80vw; height: 80vh">
+            <CustSearchModal @custreg="handleCustRegister" />
+            <!-- @select="handleCustSelect"-->
+        </Dialog>
+
+        <Dialog v-model:visible="modelmodalVisible" header="제품 검색" modal style="width: 80vw; height: 80vh">
+            <ModalSearchModel @register="handleModelRegister" />
+        </Dialog>
 
         <DataTable :value="selectedinordmodel" v-model:selection="selectedRows" scrollable scrollHeight="400px" style="height: 40vh; border: 1px solid #ddd">
             <Column selectionMode="multiple" style="width: 3rem"></Column>
